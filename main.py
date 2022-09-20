eb07aca6ca5bd990daba70da57dacf42ea6cd546
Maxim Ermolaev <ermolaev.me@jhvn.tk>
2022-09-20 21:17:27 +0300

    Add check for spaces

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ if __name__ == '__main__':
     print('\tВведите строку, чтобы "перевернуть" ее:')
     input_string = input()
     reversed_string = reverse_string(input_string)
-    if not reversed_string:
+    if reversed_string == -1:
         print('\tТы ничего не ввел :(')
+    elif reversed_string == -2:
+        print('\tТы пробелы что ли ввел там?')
     else:
         print(f'\tПеревернутая строка: {reversed_string}')
--- reverse_function.py
+++ reverse_function.py
@@ -1,4 +1,9 @@
-def reverse_string(string: str) -> str:
+from typing import Union
+
+
+def reverse_string(string: str) -> Union[str, int]:
     if not string:
-        return False
+        return -1
+    if not len(string.strip()):
+        return -2
     return ''.join(reversed(string))
